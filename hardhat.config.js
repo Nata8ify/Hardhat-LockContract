5d9376aebe7ca504e8fa2c22dde586b5d4bfa926
NP <nata6ify@gmail.com>
2024-12-01 16:37:48 +0700

    update build

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// const LOCK_CONTRACT_TESTPJ_NODE_ENDPOINT = vars.get("LOCK_CONTRACT_TESTPJ_NODE_ENDPOINT");
+// const LOCK_CONTRACT_TESTPJ_OWNER_CONTRACT_ADDRESS = vars.get("LOCK_CONTRACT_TESTPJ_OWNER_CONTRACT_ADDRESS");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    // in built test network to use when developing contracts
+    // hardhat: {
+    //     chainId: 1337
+    // },
+    besuWallet: {
+      url: vars.get("LOCK_CONTRACT_TESTPJ_NODE_ENDPOINT"),
+      chainId: 1337,
+      // test accounts only, all good ;)
+      accounts: [
+        vars.get("LOCK_CONTRACT_TESTPJ_OWNER_CONTRACT_ADDRESS")
+      ]
+    }
+  }
 };
