d5d7aab74d5935a50974a296c90ccf2a0ec52a2b
nata6ify@gmail.com <nata6ify@gmail.com>
2024-12-02 16:43:33 +0700

    add optional chain id (5)

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     //     chainId: 1337
     // },
     besuWallet: {
-      url: "http://172.18.0.1:8545",
+      url: vars.get("LOCK_CONTRACT_TESTPJ_NODE_ENDPOINT"),
       chainId: 23354,
       // test accounts only, all good ;)
       accounts: [
